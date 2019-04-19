cc9d01ee08139b287e369d7d3e929d0812ab71a7
SnowSurfer <drinkaziu@gmail.com>
2019-04-19 09:57:03 -0400

    add loop for boxes

--- src/ColorBoxes.js
+++ src/ColorBoxes.js
@@ -3,25 +3,19 @@ import Box from './Box';
 import './ColorBoxes.css';
 
 class ColorBoxes extends Component {
+  
+  static defaultProps = {
+    numBoxes: 18
+  }
+
   render() {
+    let boxes = []
+    for (let i = 0; i < this.props.numBoxes; i++) {
+      boxes.push(<Box />)
+    }
     return (
       <div className="ColorBoxes">
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
+        {boxes}
       </div>
     )
   }
